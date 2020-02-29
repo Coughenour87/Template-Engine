12dc0becd9faedc47ad98d5cdfc14abe48fb2d24
Jordan Coughenour <jcoughenour18@gmail.com>
2020-02-29 12:23:34 -0500

    removed github constructor.. will have to see if it belongs in another js file

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -6,5 +6,17 @@ class Employee {
         this.email = email;
         this.github = github;
     }
-    
+    getName() {
+        return this.name;
+    }
+    getId () {
+        return this.id;
+    }
+    getEmail () {
+        return this.email;
+    }
+    getGithub () {
+        return this.github
+    }
 }
+module.exports = Employee
