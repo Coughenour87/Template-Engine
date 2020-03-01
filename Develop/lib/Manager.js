be8f6a2a4abbcbc5f1977571920d0f4f05017220
Jordan Coughenour <jcoughenour18@gmail.com>
2020-03-01 18:25:52 -0500

    trying to run tests that done fail. Got 11 to pass.

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -3,15 +3,11 @@ const Employee = require("./Employee");
 class Manager extends Employee {
     constructor(name, id, email, officeNumber) {
         super(name, id, email, officeNumber);
-
         this.officeNumber = officeNumber;
         this.role = "Manager";
     }
     getOfficeNumber() {
         return this.getOfficeNumber;
-    }
-    getRole() {
-        return this.getRole;
     }
 }
 module.exports = Manager;
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Template-Engine
 
 ![Photo of finished Product](./Assets/fin.png)
+![Photo of finished Product](./Assets/fin1.png)
 
 * The application will prompt the user for information about the team manager and then information about the team members. 
 * The user can input any number of team members, and they may be a mix of engineers and interns.
