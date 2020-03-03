778d05215d593988ec7be21c0c1670b17acda30a
Jordan Coughenour <jcoughenour18@gmail.com>
2020-03-03 17:27:00 -0500

    updated readMe and corrected app.js

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 ![Photo of finished Product](./Assets/fin1.png)
 ![Photo of finished Product](./Assets/fin2.png)
 ![Photo of finished Product](./Assets/fin3.png)
+![Photo of finished Product](./Assets/fin4.png)
+![Photo of finished Product](./Assets/fin5.png)
 
 * The application will prompt the user for information about the team manager and then information about the team members. 
 * The user can input any number of team members, and they may be a mix of engineers and interns.
---
+++ app.js
@@ -0,0 +1,164 @@
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
+
+const OUTPUT_DIR = path.resolve(__dirname, 'output');
+const outputPath = path.join(OUTPUT_DIR, 'team.html');
+
+const render = require('./lib/htmlRenderer');
+const teamMembers = []
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+
+
+
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work!```
+
+const init = () => {
+    const addEng =()=>{
+        inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Enter Engineers name",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "Engineers email address",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "Enter Engineers id",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "Enter Engineers Github username",
+                name: "username"
+            }
+        ]).then(answers => {
+            const engineer = new Engineer(answers.name, answers.id, answers.email, answers.username)
+            teamMembers.push(engineer)
+            buildTeam()
+        })
+    }
+    const addInt=()=>{
+        inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Enter Intern name",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "Intern email address",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "Enter Intern id",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "Enter Intern School",
+                name: "username"
+            }
+        ]).then(answers => {
+            const intern = new Intern(answers.name, answers.id, answers.email, answers.username)
+            teamMembers.push(intern)
+            buildTeam()
+        })
+    }
+    const generateChart =()=>{
+        if(!fs.existsSync(OUTPUT_DIR)){
+            fs.mkdirSync(OUTPUT_DIR)
+        }
+        fs.writeFile(outputPath, render(teamMembers), "utf-8", (err)=>{
+            if (err) throw err
+            console.log("...Saving")
+        })
+    }
+    const buildTeam = ()=>{
+        inquirer.prompt([{
+            type: "list",
+            name: "whatNext",
+            message: "What type of team member would you like?",
+            choices: ["Engineer", "Intern","Generate Chart"]
+        }]).then(answers=>{
+            switch (answers.whatNext) {
+                case "Engineer":
+                    addEng()
+                    break;
+                case "Intern":
+                    addInt()
+                    break
+                default:
+                    generateChart()
+                    break;
+            }
+        })
+    }
+    const manager = () => {
+
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "Enter your name",
+                    name: "name"
+                },
+                {
+                    type: "input",
+                    message: "Enter email address",
+                    name: "email"
+                },
+                {
+                    type: "input",
+                    message: "Enter managers id",
+                    name: "id"
+                },
+                {
+                    type: "input",
+                    message: "Enter managers office number",
+                    name: "num"
+                }
+            ]).then(answers => {
+                const manager = new Manager(answers.name, answers.id, answers.email, answers.num)
+                teamMembers.push(manager)
+                buildTeam()
+            })
+    }
+
+    manager()
+
+};
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above to target this location.
+
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not. The fs npm package may have methods to check if a directory exists, and they
+// may also have methods to create a directory that doesn't...
+
+init();
